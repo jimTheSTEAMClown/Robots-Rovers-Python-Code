1ea85e84fa16599abaa0d79c18068cfd8c940bfd
Jim Burnham <jim.the.steam.clown@gmail.com>
2026-01-31 16:02:49 -0800

    Create generic-python-start-here-template.py

---
+++ Templates/generic-python-start-here-template.py
@@ -0,0 +1,18 @@
+# Global Variables & Imported Libraries
+my_global_int = 67
+print(my_global_int)
+
+def main():
+  print("Starting The Code Challenge")
+  # Declare Any main() Variables - (Not Global)
+
+  print("Starting <Specific Part> Of Code Challenge")
+  # Enter Your Code Here:
+  print('Enter Your Code Here:')
+  x = 42
+  print(x)
+  print("Ending <Specific Part> Of Code Challenge")
+  print("Ending The Code Challenge")
+
+# Calling main() funtion
+main()
