4a34e5eb86d3bed78650d2fdad93235d00137e9f
Jim Burnham <jim.the.steam.clown@gmail.com>
2026-02-21 13:12:30 -0800

    Update rover_pass_diectionaries_functions_lab1.py

--- functions/rover_pass_diectionaries_functions_lab1.py
+++ functions/rover_pass_diectionaries_functions_lab1.py
@@ -1,4 +1,13 @@
 def rover_battery_status():
+    '''STEP1 - Enter code here to update key:value pairs.
+       hard code or get user input to change the
+       value for the following:
+         "status": "unknown"
+         "value": None
+       This will mean you are creating some variables
+       like, battery_status = ? and battery_value = ?
+       and relpacing the hardcoded string "unknown" and None
+    '''
     return {
         "name": "battery",
         "status": "unknown",
@@ -12,8 +21,15 @@ def rover_sensor_status():
     }
 
 def main():
+    ''' STEP 2 - Update your code to extract and print the actual
+        values that are returned.  The expected output should be
+        something like:
+        "Battery sensor status = Good, with a reading of 11.2 Volts"
+        where "Battery" is the key for the returend sensor and "Good" is the value of the key 'status' and "11.2" is the
+        value of 'value' and "Volts" is the value of the key 'unit'
+    '''
     print(rover_sensor_status())
-
 
 if __name__ == "__main__":
     main()
+
