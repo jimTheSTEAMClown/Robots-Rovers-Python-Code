9886c12229bca6bd03d07479a9b6b488d5d158d6
Jim Burnham <jim.the.steam.clown@gmail.com>
2026-02-21 13:26:21 -0800

    Create dictionary_my_cat_lab2.py

---
+++ dictionaries/dictionary_my_cat_lab2.py
@@ -0,0 +1,6 @@
+my_cat = {'size':'extra medium', 'color':'turtle shell', 'disposition':'loud', 'age':13}
+print(my_cat)
+print(my_cat['size'])
+print('My cat has ' + my_cat['color'] + ' fur')
+# How do you fix this Traceback Error? What is the type/class of the :value of 'age'
+print('My cat is ' + my_cat['age'] + ' years old')
